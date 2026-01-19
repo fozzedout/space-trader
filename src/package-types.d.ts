15422193cc2a38c8130346aa40b96fcba35bd246
Jaxah <fozzedout@gmail.com>
2026-01-19 11:51:30 +0000

    project wipe

--- src/package-types.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Type definitions for packages that may not have perfect types in this environment
- */
-
-declare module "seedrandom" {
-  export default function seedrandom(seed?: string): () => number;
-}
